090d1dfc53eeb9c1173bdce5c9e5f8421bac1310
roni <ronikauppinen01@gmail.com>
2026-01-29 08:58:11 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+const text = document.getElementById("scrollText");
+let pos = 0;
+const speed = 1;
 
+function scroll() {
+  pos -= speed;
+  text.style.transform = `translateX(${pos}px)`;
+
+  if (pos <= -text.offsetWidth) {
+    pos = window.innerWidth;
+  }
+
+  requestAnimationFrame(scroll);
+}
+
+scroll();
