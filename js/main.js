afba80f57d9bbb3a1927e6661ed9a2a62471e373
roni <ronikauppinen01@gmail.com>
2026-04-21 12:22:22 +0300

    lets see if this works

--- js/main.js
+++ js/main.js
@@ -17,28 +17,28 @@ scroll();
 
 
 const images = [
-  "/assets/a48.jpg",
-  "/assets/info13.jpg",
-  "/assets/info27.jpg",
-  "/assets/info74.jpg",
-  "/assets/info72.jpg",
-  "/assets/info47.jpg",
-  "/assets/info17.jpg",
-  "/assets/a52.jpg",
-  "/assets/G1.png",
-  "/assets/info55.jpg",
-  "/assets/info80.jpg",
-  "/assets/info38.jpg",
-  "/assets/test.jpg",
-  "/assets/info77.jpg",
-  "/assets/info30.jpg",
-  "/assets/info42.jpg",
-  "/assets/Drawing14.jpg",
-  "/assets/color38.jpg",
-  "/assets/info86.jpg",
-  "/assets/info90.jpg",
-  "/assets/info91.jpg",
-  "/assets/info89.jpg"
+  "assets/a48.jpg",
+  "assets/info13.jpg",
+  "assets/info27.jpg",
+  "assets/info74.jpg",
+  "assets/info72.jpg",
+  "assets/info47.jpg",
+  "assets/info17.jpg",
+  "assets/a52.jpg",
+  "assets/G1.png",
+  "assets/info55.jpg",
+  "assets/info80.jpg",
+  "assets/info38.jpg",
+  "assets/test.jpg",
+  "assets/info77.jpg",
+  "assets/info30.jpg",
+  "assets/info42.jpg",
+  "assets/Drawing14.jpg",
+  "assets/color38.jpg",
+  "assets/info86.jpg",
+  "assets/info90.jpg",
+  "assets/info91.jpg",
+  "assets/info89.jpg"
 ];
 
 let currentIndex = -1;
@@ -63,25 +63,3 @@ function setRandomBackground() {
 }
 setRandomBackground();
 setInterval(setRandomBackground, 30000);
-
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
-const closeBtn = document.getElementById("lightbox-close");
-
-document.querySelectorAll(".gallery img").forEach(img => {
-  img.style.cursor = "pointer";
-  img.onclick = () => {
-    lightbox.style.display = "flex";
-    lightboxImg.src = img.src;
-  };
-});
-
-closeBtn.onclick = () => {
-  lightbox.style.display = "none";
-};
-
-lightbox.onclick = (e) => {
-  if (e.target !== lightboxImg) {
-    lightbox.style.display = "none";
-  }
-};
