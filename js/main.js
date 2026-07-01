1919ff825681868fa5879e93dc8d4afbfd76fc86
roni <ronikauppinen01@gmail.com>
2026-07-01 08:48:07 +0300

    new art

--- js/main.js
+++ js/main.js
@@ -44,7 +44,8 @@ const images = [
   "assets/info95.jpg",
   "assets/info98.jpg",
   "assets/info111.jpg",
-  "assets/info114.jpg"
+  "assets/info114.jpg",
+  "assets/info130.jpg"
 ];
 
 let currentIndex = -1;
